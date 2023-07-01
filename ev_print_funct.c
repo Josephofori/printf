1eb592a65f689167522ea54453672ca15e508ae6
Joseph Ofori <josephofori75@gmail.com>
2023-07-01 00:12:48 -0700

    update ev_print_funct.c

--- ev_print_funct.c
+++ ev_print_funct.c
@@ -9,10 +9,9 @@
 int ev_print_funct(const char *s, int index)
 {
 	print_t pr[] = {
-		{"c", print_chr}, {"s", print_str}, {"i", print_int},
+		{"c", print_char}, {"s", print_str}, {"i", print_int},
 		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
-		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
-		{" %", print_prg},
+		{"o", print_oct}, {"x", print_hex}, {" %", print_prg},
 		{NULL, NULL},
 };
 int a = 0, b = 0, first_index;
